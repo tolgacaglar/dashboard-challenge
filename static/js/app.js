0c6088d158c5a043753f3f9eddda2e6a0045c907
Tolga Caglar <tolcaglar@gmail.com>
2021-03-16 19:42:43 -0700

    delete gauge plot

--- static/js/app.js
+++ static/js/app.js
@@ -59,19 +59,6 @@ function optionChanged(value) {
 
 
     // Gauge chart
-    var data = [
-      {
-        domain: { x: [0, 1], y: [0, 1] },
-        value: 270,
-        title: { text: "Speed" },
-        type: "indicator",
-        mode: "gauge+number"
-      }
-    ];
-    
-    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-    Plotly.newPlot('myDiv', data, layout);
-
 
     // Add table to the demographic area
     var metadata = data["metadata"];
